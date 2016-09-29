887ae1ad4d5cf98fe246685ac2173291443ab1a6
RAKSHA <RAKSHA.MUTH@GMAIL.COM>
2016-09-29 19:52:50 -0400

    changes URL with button clicks

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,48 +1,126 @@
+{- This app is the basic counter app. You can increment and decrement the count
+like normal. The big difference is that the current count shows up in the URL.
+Try changing the URL by hand. If you change it to a number, the app will go
+there. If you change it to some invalid address, the app will recover in a
+reasonable way.
+-}
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as Html
 import Html.Events exposing ( onClick )
+import Navigation
+import String
 
 -- component import example
 import Components.Hello exposing ( hello )
 
-
--- APP
-main : Program Never
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Navigation.program urlParser
+    { init = init
+    , view = view
+    , update = update
+    , urlUpdate = urlUpdate
+    , subscriptions = subscriptions
+    }
+
+
+
+-- URL PARSERS - check out evancz/url-parser for fancier URL parsing
+
+
+toUrl : Int -> String
+toUrl count =
+  "#/" ++ toString count
+
+
+fromUrl : String -> Result String Int
+fromUrl url =
+  String.toInt (String.dropLeft 2 url)
+
+
+urlParser : Navigation.Parser (Result String Int)
+urlParser =
+  Navigation.makeParser (fromUrl << .hash)
+
 
 
 -- MODEL
+
+
 type alias Model = Int
 
-model : number
-model = 0
+
+init : Result String Int -> (Model, Cmd Msg)
+init result =
+  urlUpdate result 0
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
 
-update : Msg -> Model -> Model
+
+type Msg = Increment | Decrement
+
+
+{-| A relatively normal update function. The only notable thing here is that we
+are commanding a new URL to be added to the browser history. This changes the
+address bar and lets us use the browser&rsquo;s back button to go back to
+previous pages.
+-}
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+  let
+    newModel =
+      case msg of
+        Increment ->
+          model + 1
+
+        Decrement ->
+          model - 1
+  in
+    (newModel, Navigation.newUrl (toUrl newModel))
+
+
+{-| The URL is turned into a result. If the URL is valid, we just update our
+model to the new count. If it is not a valid URL, we modify the URL to make
+sense.
+-}
+urlUpdate : Result String Int -> Model -> (Model, Cmd Msg)
+urlUpdate result model =
+  case result of
+    Ok newCount ->
+      (newCount, Cmd.none)
+
+    Err _ ->
+      (model, Navigation.modifyUrl (toUrl model))
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
+    div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
     div [ class "row" ][
       div [ class "col-xs-12" ][
         div [ class "jumbotron" ][
           img [ src "static/img/elm.jpg", style styles.img ] []                                    -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
+          , div
+            [ class "h2" ]
+            [ text ( "Connecting Activists with Supporters" ) ]                                                                     -- ext 'hello' component (takes 'model' as arg)
           , p [] [ text ( "Find Out How You Can Help" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
+          , button [ class "btn btn-primary btn-lg", onClick Decrement ] [                  -- click handler
             span[ class "" ][]                                      -- glyphicon
             , span[][ text "Find a protest" ]
           ]
@@ -54,7 +132,6 @@ view model =
       ]
     ]
   ]
-
 
 -- CSS STYLES
 styles : { img : List ( String, String ) }
